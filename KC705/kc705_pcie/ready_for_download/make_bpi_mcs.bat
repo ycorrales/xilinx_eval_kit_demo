fc00278e2d686756acd85d3e749e9d56886e4681
maps <maps@localhost.localdomain>
2021-10-21 13:13:47 -0600

    ycm - update kc705_pcie

--- KC705/kc705_pcie/ready_for_download/make_bpi_mcs.bat
+++
@@ -1,5 +0,0 @@
-call C:\\Xilinx\\Vivado\\2014.3\\.\\bin\\vivado.bat -mode batch -source make_bpi_mcs.tcl
-if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
-if exist vivado_*.backup.jou del /F vivado_*.backup.jou
-if exist vivado_*.backup.log del /F vivado_*.backup.log
-pause
